3ed83b8c8685abdd383512302a107ad1e7ea3e7b
Alex <alex.r@gms.info>
2022-10-25 10:51:56 +0300

    commit 2

---
+++ resources/views/app/welcome.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+
+@section('content')
+            <div class="orb-content" data-swiper-parallax="-200" data-swiper-parallax-opacity="0">
+
+                <div class="orb-grid orb-sm-reverse">
+                    <div class="orb-g-100">
+                        <div class="rb-object-frame">
+                            <div class="orb-photo-frame orb-obj-1">
+                                <div class="swiper-container orb-slideshow">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <img src="/html/img/photo/1.jpg" alt="photo" data-swiper-parallax-x="100" data-swiper-parallax-scale="1.3" data-swiper-parallax-opacity="0"
+                                                 data-swiper-parallax-duration="1200">
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="/html/img/photo/2.jpg" alt="photo" data-swiper-parallax-x="100" data-swiper-parallax-scale="1.3" data-swiper-parallax-opacity="0"
+                                                 data-swiper-parallax-duration="1200">
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="/html/img/photo/3.jpg" alt="photo" data-swiper-parallax-x="100" data-swiper-parallax-scale="1.3" data-swiper-parallax-opacity="0"
+                                                 data-swiper-parallax-duration="1200">
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="/html/img/photo/4.jpg" alt="photo" data-swiper-parallax-x="100" data-swiper-parallax-scale="1.3" data-swiper-parallax-opacity="0"
+                                                 data-swiper-parallax-duration="1200">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="orb-deco"></div>
+                            <div class="orb-slide-caption">
+                                <span class="orb-suptitle">Professional <br>photographers</span>
+                                <h1><span>Alexander<br>&amp; Alice&nbsp; </span></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+@endsection
+
+
+
