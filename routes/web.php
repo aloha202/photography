a9489237f148c4501b0cd5e117abc8ad6e4b2c19
Alex <alex.r@gms.info>
2022-11-27 11:43:55 +0200

    errors

--- routes/web.php
+++ routes/web.php
@@ -72,8 +72,8 @@ Route::get('/discount', function (){
     return view('app.comming', ['wrapper_class' => 'orb-contact-1', 'current' => 'discount']);
 });
 
-Route::get('/message-sent', function (){
-    return view('app.message-sent', ['result' => session('result')]);
+Route::get('/error', function (){
+    throw new \Exception('Something went wrong');
 });
 
 Route::get('/reserve', function (){
